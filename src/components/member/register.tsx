e1a06d436149eb9072690f045170b18abe8908e1
jehee <ljh945999@gmail.com>
2024-10-19 23:31:49 +0900

    add : register check duplicate id

--- src/components/member/register.tsx
+++ src/components/member/register.tsx
@@ -14,7 +14,10 @@ import {
 } from '../../styles/register';
 import { FormData } from '../../interface/memberForm';
 
+
+
 const SignupForm: React.FC = () => {
+  const [isIdAvailable, setIsIdAvailable] = useState<boolean>(false);
   const [formData, setFormData] = useState<FormData>({
     id: '',
     pw: '',
@@ -29,10 +32,30 @@ const SignupForm: React.FC = () => {
   const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+   
+    if (name === 'id') {
+      setIsIdAvailable(false);
+    }
+  };
+
+  const handleDuplicateCheck = async () => {
+    try {
+      const response = await axios.get(`/api/auth/member/duplicate?loginId=${formData.id}`);
+      setIsIdAvailable(!response.data);
+      alert(response.data ? '중복된 아이디입니다.' :'사용 가능한 아이디입니다.');
+    } catch (error) {
+      console.error('중복 확인 중 오류 발생:', error);
+    }
   };
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault();
+
+    if (!isIdAvailable) {
+      alert('중복 확인이 필요합니다. 아이디를 확인해 주세요.');
+      return;
+    }
+
     try {
       const response = await axios.post('/api/auth/member', formData);
       alert(`회원가입이 성공했습니다!\n아이디: ${formData.id}\n이름: ${formData.name}\n닉네임: ${formData.nickname}\n전화번호: ${formData.phone}\n학번: ${formData.scode}\n이메일: ${formData.email}`);
@@ -61,7 +84,7 @@ const SignupForm: React.FC = () => {
             onChange={handleChange}
             required
           />
-          <DuplicateCheckButton>중복확인</DuplicateCheckButton>
+          <DuplicateCheckButton onClick={handleDuplicateCheck}>중복확인</DuplicateCheckButton>
 
           <InputLabel htmlFor="pw">비밀번호</InputLabel>
           <InputField
